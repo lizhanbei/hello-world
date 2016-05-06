a6bbb2c4d741578370957a799470382a88056680
lizhanbei <lizhanbei777@163.com>
2016-05-06 16:43:04 +0800

    add welcome activity

---
+++ app/src/main/java/com/bei/test/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.bei.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lizhanbei on 16/5/6.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
